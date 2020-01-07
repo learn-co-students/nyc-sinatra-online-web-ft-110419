55a2eec2f1401ec6961147d3953532ae3c4d123c
jessesbyers <jesse.s.byers@gmail.com>
2020-01-07 15:44:37 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -20,7 +20,7 @@ class FiguresController < ApplicationController
     end
 
     if params[:title][:name] != ""
-      figure.titles << Title.create(:name => params[:title][:name])
+      figure.titles << Title.find_or_create_by(:name => params[:title][:name])
     end
 
     landmark_ids = params[:figure][:landmark_ids]
@@ -30,9 +30,50 @@ class FiguresController < ApplicationController
     end
 
     if params[:landmark][:name] != "" && params[:landmark][:year] != ""
-      figure.landmarks << Landmark.create(:name => params[:landmark][:name], :year => params[:landmark][:year])
+      figure.landmarks << Landmark.find_or_create_by(:name => params[:landmark][:name], :year_completed => params[:landmark][:year])
     end
 
     figure.save
   end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/edit'
+  end
+
+  # patch '/figures/:id' do
+  #   binding.pry
+  #   @figure = Figure.find_by_id(params[:id])
+  #   @figure.name = params[:figure][:name]
+
+  #   title_ids = params[:figure][:title_ids]
+  #   title_ids.each do |id|
+  #      @figure.titles << Title.find_or_create_by(:id => id)
+  #   end
+
+  #   if params[:title][:name] != ""
+  #     @figure.titles << Title.find_or_create_by(:name => params[:title][:name])
+  #   end
+
+  #   landmark_ids = params[:figure][:landmark_ids]
+  #   landmark_ids.each do |id|
+  #      @figure.landmarks << Landmark.find_or_create_by(:id => id)
+  #     # Landmark.find_or_create_by(:id => id)
+  #   end
+
+  #   if params[:landmark][:name] != "" && params[:landmark][:year] != ""
+  #     @figure.landmarks << Landmark.find_or_create_by(:name => params[:landmark][:name], :year_completed => params[:landmark][:year])
+  #   end
+
+  #   @figure.save
+
+  # end
+
 end
